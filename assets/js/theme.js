a36eed8eb2111fab038ed799886bd750fcb53e2e
Kristian3333 <kristiangbarman@gmail.com>
2023-10-08 12:43:37 +0200

    changing default to dark

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -71,13 +71,14 @@ let transTheme = () => {
 let initTheme = (theme) => {
   if (theme == null || theme == 'null') {
     const userPref = window.matchMedia;
-    if (userPref && userPref('(prefers-color-scheme: dark)').matches) {
-        theme = 'dark';
+    if (userPref && userPref('(prefers-color-scheme: light)').matches) {
+        theme = 'light';
+    } else {
+        theme = 'dark'; // Set the default theme to dark.
     }
   }
 
   setTheme(theme);
 }
-
 
 initTheme(localStorage.getItem("theme"));
